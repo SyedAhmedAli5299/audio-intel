68e559475f56eba03fe968cf3c5c9b0c0d285d20
SyedAhmedAli5299 <syedahmedali534@gmail.com>
2025-09-17 12:16:30 +0500

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "localhost",
+      "127.0.0.1",
+      "8a4c13f1-5ae7-4134-9b90-410a4e846de0-00-34vpvimrigkpl.janeway.replit.dev",
+    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
